51efadc4f8164db58ff9af1456d0cf992c509fa7
Thinh Tran <48557139+ThinhTranA@users.noreply.github.com>
2021-07-05 21:51:42 +0800

    Add fluentassertions.

--- Catalog.UnitTests/ItemsControllerTests.cs
+++ Catalog.UnitTests/ItemsControllerTests.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Threading.Tasks;
 using Catalog.Api.Controllers;
+using Catalog.Api.Dtos;
 using Catalog.Api.Entities;
 using Catalog.Api.Repositories;
 using DnsClient.Internal;
+using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Moq;
@@ -12,17 +14,18 @@ using Xunit;
 namespace Catalog.UnitTests
 {
 	public class ItemsControllerTests
-	{
+	{ 
+		private readonly Mock<IItemsRepository> repositoryStub = new(); 
+		private readonly Mock<ILogger<ItemsController>> loggerStub = new();
+		private readonly Random rand = new();
+		
 		[Fact]
 		//public void UnitOfWork_StateUnderTest_ExpectedBehavior()
 		public async Task GetItemAsync_WithUnexistingItem_ReturnsNotFound()
 		{
 			// Arrange
-			var repositoryStub = new Mock<IItemsRepository>();
 			repositoryStub.Setup(repo => repo.GetItemAsync(It.IsAny<Guid>()))
 				.ReturnsAsync((Item)null); //cast to null to Item so that Moq doesn't get confused
-
-			var loggerStub = new Mock<ILogger<ItemsController>>();
 
 			var controller = new ItemsController(repositoryStub.Object, loggerStub.Object);
 
@@ -30,7 +33,46 @@ namespace Catalog.UnitTests
 			var result = await controller.GetItemAsync(Guid.NewGuid());
 
 			// Assert
-			Assert.IsType<NotFoundResult>(result.Result);
+			result.Result.Should().BeOfType<NotFoundResult>();
 		}
+		
+		[Fact]
+		//public void UnitOfWork_StateUnderTest_ExpectedBehavior()
+		public async Task GetItemAsync_WithExistingItem_ReturnsExpectedItem()
+		{
+			// Arrange
+			var expectedItem = CreateRandomItem();
+			repositoryStub.Setup(repo => repo.GetItemAsync(It.IsAny<Guid>()))
+				.ReturnsAsync(expectedItem);
+
+			var controller = new ItemsController(repositoryStub.Object, loggerStub.Object);
+			
+			// Act
+			var result = await controller.GetItemAsync(Guid.NewGuid());
+
+			// Assert
+			Assert.IsType<ItemDto>(result.Value);
+			var dto = result.Value;
+			
+			result.Value.Should().BeEquivalentTo(
+				expectedItem,
+				options => options.ComparingByMembers<Item>());
+			//Good practice is should have only 1 assertion. 
+			// Assert.Equal(expectedItem.Id, dto.Id);
+			// Assert.Equal(expectedItem.Name, dto.Name);
+			// Assert.Equal(expectedItem.Price, dto.Price);
+		}
+
+		private Item CreateRandomItem()
+		{
+			return new()
+			{
+				Id = Guid.NewGuid(),
+				Name = Guid.NewGuid().ToString(),
+				Price = rand.Next(1000),
+				CreatedDate = DateTimeOffset.UtcNow
+			};
+		}
+
 	}
 }
